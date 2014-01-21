7e823b8a6d69989682b4c3e15663adc638abca9b
Mac User <macuser@macs-MacBook-4.local>
2014-01-21 17:59:22 -0500

    Initial Commit

---
+++ Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Mac User on 1/21/14.
+//  Copyright (c) 2014 Mac User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Mac User on 1/21/14.
+//  Copyright (c) 2014 Mac User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QuizAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QuizAppDelegate class]));
+    }
+}
