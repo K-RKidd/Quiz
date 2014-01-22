994a6fcca57cc7441761436bd79e2dc60f0cd015
Mac User <macuser@macs-MacBook-4.local>
2014-01-21 21:40:05 -0500

    first commit

--- Quiz/QuizViewController.h
+++ Quiz/QuizViewController.h
@@ -3,11 +3,29 @@
 //  Quiz
 //
 //  Created by Mac User on 1/21/14.
-//  Copyright (c) 2014 Mac User. All rights reserved.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface QuizViewController : UIViewController
+
+{
+    int currentQuestionIndex;
+    
+    //The model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    //The view objects
+    IBOutlet  UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
+
+
+
+
 
 @end
